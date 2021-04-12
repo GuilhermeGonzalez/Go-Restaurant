fe21ad7fade8c0ebfdad0d0bafd83e4281823218
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-04-12 15:22:58 -0300

    refactor: ajuste nos arquivos main para .tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+const App = (): JSX.Element => {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  );
+}
+
+export default App;
